077b0f93f02c931d3ec4ddb6240e73d0cda2354e
TimTimaTim <timuribr17@gmail.com>
2026-03-03 16:53:21 +0100

    q7 added

--- q7.py
+++ q7.py
@@ -5,17 +5,16 @@ random_numbers = []
 for i in range(10):
     random_numbers.append(random.randint(1, 100))
 
-# Go through the list using index because we want to modify values
+# look at the list using the index
 for i in range(len(random_numbers)):
 
-    # If number is odd
+    # if number is odd replace it with its negative value
     if random_numbers[i] % 2 != 0:
-        # Replace it with its negative value
         random_numbers[i] = -random_numbers[i]
 
     else:
-        # If number is even, replace it with double value
+        # if even replace it with double value
         random_numbers[i] = random_numbers[i] * 2
 
-# Print final list
+# prints final list
 print(random_numbers)
